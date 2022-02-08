4df6adb912b2c59094fac0854d1504147ac3ed71
Edmund <edmundjet99@gmail.com>
2022-02-08 15:39:32 -0800

    foundation

---
+++ server/routes/posts.js
@@ -0,0 +1,13 @@
+import PostMessage from '../models/postMessage.js'
+
+import express from 'express';
+
+import { getPosts, createPost } from '../controllers/posts.js';
+
+
+const router = express.Router();
+
+router.get('/', getPosts);
+router.post('/', createPost);
+
+export default router;
