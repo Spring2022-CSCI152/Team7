16e028ee3c05f2ae8c8e83d48318b025673884a5
Edmund <edmundjet99@gmail.com>
2022-02-21 01:39:21 -0800

    user can log in, but username not display, shit not working, send help

--- flutter_application_1/lib/responsive/mobile_screen_layout.dart
+++ flutter_application_1/lib/responsive/mobile_screen_layout.dart
@@ -1,14 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<MobileScreenLayout> createState() => _MobileScreenLayout();
+}
+
+class _MobileScreenLayouState extends State<MobileScreenLayout> {
+String username = "";
+
+@override
+  void initState() {
+    super.initState();
+    getUsername();
+  }
+
+void getUsername() async {
+  DocumentSnapshot snap = await FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid).get();
+
+
+  setState(() {
+    username = (snap.data() as Map<String, dynamic>)['username'];
+  });
+}
+
+
+  @override
+  Widget build(BuildContext context){
     return Scaffold(
-      body: Center(
-        child: Text('This is mobile'),
+      body:Center{
+        child: Text('$username'),
       ),
-    );
+    };
   }
 }
