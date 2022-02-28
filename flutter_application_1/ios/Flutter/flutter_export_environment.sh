a11cc131b39da8b6b1d37755d6500cc848a6f478
Evan Madrigal <51463735+EvanMadrigal@users.noreply.github.com>
2022-02-27 20:40:36 -0800

    packages

---
+++ flutter_application_1/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/2.10.1/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/evanmadrigal/Documents/GitHub/Team7/Team7/flutter_application_1"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
