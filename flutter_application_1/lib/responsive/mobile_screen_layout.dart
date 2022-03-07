163f31bea3c0c97992c5c0ba815c022d2b1d82a5
Edmund <edmundjet99@gmail.com>
2022-03-06 16:34:05 -0800

    added smooth_scrolling, big with screen change, working on upload page

--- flutter_application_1/lib/responsive/mobile_screen_layout.dart
+++ flutter_application_1/lib/responsive/mobile_screen_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/colors.dart';
+import 'package:flutter_application_1/utils/global_variables.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
@@ -29,19 +30,19 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
     pageController.jumpToPage(page);
   }
 
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
-        children: [
-          Text('feed'),
-          Text('search'),
-          Text('add Posts'),
-          Text('notification'),
-          Text('profile'),
-        ],
+        children: homeScreenItems,
         controller: pageController,
-        // onPageChanged: onPageChanged,
+        onPageChanged: onPageChanged,
       ),
       bottomNavigationBar: CupertinoTabBar(
         backgroundColor: mobileBackgroundColor,
@@ -49,7 +50,7 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
           BottomNavigationBarItem(
             icon: Icon(
               Icons.home,
-              color: _page == 0 ? primaryColor : secondaryColor,
+              color: (_page == 0) ? primaryColor : secondaryColor,
             ),
             label: '',
             backgroundColor: primaryColor,
@@ -57,7 +58,7 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
           BottomNavigationBarItem(
             icon: Icon(
               Icons.search,
-              color: _page == 1 ? primaryColor : secondaryColor,
+              color: (_page == 1) ? primaryColor : secondaryColor,
             ),
             label: '',
             backgroundColor: primaryColor,
@@ -65,7 +66,7 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
           BottomNavigationBarItem(
             icon: Icon(
               Icons.add_circle,
-              color: _page == 2 ? primaryColor : secondaryColor,
+              color: (_page == 2) ? primaryColor : secondaryColor,
             ),
             label: '',
             backgroundColor: primaryColor,
@@ -73,7 +74,7 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
           BottomNavigationBarItem(
             icon: Icon(
               Icons.favorite,
-              color: _page == 3 ? primaryColor : secondaryColor,
+              color: (_page == 3) ? primaryColor : secondaryColor,
             ),
             label: '',
             backgroundColor: primaryColor,
@@ -81,13 +82,14 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
           BottomNavigationBarItem(
             icon: Icon(
               Icons.person,
-              color: _page == 4 ? primaryColor : secondaryColor,
+              color: (_page == 4) ? primaryColor : secondaryColor,
             ),
             label: '',
             backgroundColor: primaryColor,
           ),
         ],
         onTap: navigationTapped,
+        currentIndex: _page,
       ),
     );
   }
---
+++ flutter_application_1/lib/screens/add_post_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AddPostScreen extends StatefulWidget {
+  AddPostScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: IconButton(
+        icon: const Icon(Icons.upload),
+        onPressed: () {},
+      ),
+    );
+  }
+}
