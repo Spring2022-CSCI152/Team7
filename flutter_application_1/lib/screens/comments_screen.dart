d4549f788efb69f34ff0d933e61b7a2b83a07212
Edmund <edmundjet99@gmail.com>
2022-04-10 12:05:25 -0700

    comments showing real time from firebase success!, view comment button not working :(

--- flutter_application_1/lib/screens/comments_screen.dart
+++ flutter_application_1/lib/screens/comments_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/user.dart';
 import 'package:flutter_application_1/providers/user_provider.dart';
@@ -33,7 +34,32 @@ class _CommentsScreenState extends State<CommentsScreen> {
         title: const Text('Comments'),
         centerTitle: false,
       ),
-      body: CommentCard(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('posts')
+            .doc(widget.snap['postId'])
+            .collection('comments')
+            .orderBy(
+              'datePublished',
+              descending: true,
+            )
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: (snapshot.data! as dynamic)
+                .docs
+                .length, //has to be confirmed from time to time (potential bug area)
+            itemBuilder: (context, index) => CommentCard(
+                snap: (snapshot.data! as dynamic).docs[index].data()),
+          );
+        },
+      ),
       bottomNavigationBar: SafeArea(
         child: Container(
           height: kToolbarHeight,
@@ -67,6 +93,11 @@ class _CommentsScreenState extends State<CommentsScreen> {
                     user.uid,
                     user.username,
                     user.photoUrl,
+                  );
+                  setState(
+                    () {
+                      _commentController.text = "";
+                    },
                   );
                 },
                 child: Container(
